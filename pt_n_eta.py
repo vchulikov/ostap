ee08530c4d4c4bcdc569b53f2cc22dff8729624c
Vladimir <51376141+vchulikov@users.noreply.github.com>
2019-10-21 12:20:03 +0300

    Update pt_n_eta.py

--- pt_n_eta.py
+++ pt_n_eta.py
@@ -2,7 +2,7 @@ import ROOT
 
 h = ROOT.TH1F("h", "h", 10, 0, 10)
 
-f = TFile.Open("test_file.root", "read")
+f = ROOT.TFile.Open("test_file.root", "read")
 obj = f.ds_k
 obj.draw('pt:ntrk')
 a = obj.y
