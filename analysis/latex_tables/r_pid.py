afdda7cfaa77fe5a82fda21b21844532c7d85cc3
Vladimir <51376141+vchulikov@users.noreply.github.com>
2020-02-14 15:08:38 +0300

    Create r_pid.py

---
+++ analysis/latex_tables/r_pid.py
@@ -0,0 +1,19 @@
+##pt + R PID + s1 + s2 + s3 + summ_err --->latex table
+import numpy
+
+if __name__ == "__main__" :
+ data = numpy.genfromtxt('data_all.csv', delimiter = ',')
+ length_x = numpy.size(data, 0)
+ srtformat="{:7.5f}";
+
+ for i in range(length_x - 1):
+  r = data[i + 1, 13]/data[i + 1, 14]
+  s1 = abs(r - data[i + 1, 15]/data[i + 1, 16])
+  s2 = abs(r - data[i + 1, 17]/data[i + 1, 18])
+  s3 = data[i + 1, 20]
+  summ_err = (s1**2+s2**2+s3**2)**0.5
+  if i%10 == 0: 
+   print(str("\hline"))
+   print("\multicolumn{6}{c}{$y\in$~(" + str(data[i + 1, 1]) + "," + str(data[i + 1, 2])+ ")}" + "\\" + "\\")
+   print(str("\hline"))
+  print("(" + str(data[i + 1, 3]) +"," + str(data[i + 1, 4]) + ") & "+ srtformat.format(r) + " & "+ srtformat.format(s1) + " & " + srtformat.format(s2) + " & " + srtformat.format(s3) + " & " + srtformat.format(summ_err)  +"\\" + "\\")
